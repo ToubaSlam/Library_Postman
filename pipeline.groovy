299f3895388ed48d316dcf6757a1a888e66f75e1
islamtoba <islamtoba@gmail.com>
2025-07-06 04:31:21 -0500

    sst Commit

--- pipeline.groovy
+++ pipeline.groovy
@@ -2,16 +2,15 @@ pipeline {
     agent any
 
     stages {
-        stage('Get Code') {
-            steps {
-                git changelog: false, poll: false, url: 'https://github.com/islamtoba/Restful_Booking_Project.git'
-            }
-        }
-
         stage('Run Test') {
             steps {
                 bat 'newman run "Library.postman_collection.json" -e Test.postman_environment.json --reporters=cli,htmlextra'
             }
+        }
+    }
+    post {
+        always {
+            archiveArtifacts artifacts: 'newman/*.html', allowEmptyArchive: true
         }
     }
 }
